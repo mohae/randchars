8ce838a557a154499c96ae811cdc02105b6436df
Joel Scoble <joel.scoble@outlook.com>
2016-08-15 20:14:21 -0500

    add csprng based random character generation

---
+++ crandchars/crandchars.go
@@ -0,0 +1,129 @@
+// Package crandchars generates a chunk of random ASCII characters using a
+// CSPRNG.  The supported ranges are: a-zA-Z0-9, a-z0-9, A-Z0-9, a-zA-Z, a-z,
+// A-Z, and a-zA-Z0-9=_.
+//
+// Calls to the package functions are threadsafe.
+package crandchars
+
+import (
+	"crypto/rand"
+	"sync"
+)
+
+const (
+	alphaNum      = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	alpha         = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	lowerAlphaNum = "abcdefghijklmnopqrstuvwxyz0123456789"
+	lowerAlpha    = "abcdefghijklmnopqrstuvwxyz"
+	upperAlphaNum = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	upperAlpha    = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	ascii64       = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789=_"
+	CacheSize     = 4096 // The number of random bytes to cache.
+)
+
+var gen *Generator
+var genMu sync.Mutex
+
+func init() {
+	gen = New()
+}
+
+// Generator handles generation of random chars.
+type Generator struct {
+	cache     []byte
+	cacheSize int
+	current   int
+}
+
+// New returns a Generator that uses the default CacheSize.
+func New() *Generator {
+	return NewGenerator(CacheSize)
+}
+
+// NewGenerator returns a generator with a cache of n bytes.
+func NewGenerator(n int) *Generator {
+	g := Generator{cache: make([]byte, n), cacheSize: n}
+	g.read()
+	return &g
+}
+
+func (g *Generator) AlphaNum(n int) []byte {
+	b := make([]byte, n)
+	for i := 0; i < n; i++ {
+		b[i] = alphaNum[g.intN(uint8(len(alphaNum)))]
+	}
+	return b
+}
+
+func (g *Generator) Alpha(n int) []byte {
+	b := make([]byte, n)
+	for i := 0; i < n; i++ {
+		b[i] = alpha[g.intN(uint8(len(alpha)))]
+	}
+	return b
+}
+
+func (g *Generator) LowerAlphaNum(n int) []byte {
+	b := make([]byte, n)
+	for i := 0; i < n; i++ {
+		b[i] = lowerAlphaNum[g.intN(uint8(len(lowerAlphaNum)))]
+	}
+	return b
+}
+
+func (g *Generator) LowerAlpha(n int) []byte {
+	b := make([]byte, n)
+	for i := 0; i < n; i++ {
+		b[i] = lowerAlpha[g.intN(uint8(len(lowerAlpha)))]
+	}
+	return b
+}
+
+func (g *Generator) UpperAlphaNum(n int) []byte {
+	b := make([]byte, n)
+	for i := 0; i < n; i++ {
+		b[i] = upperAlphaNum[g.intN(uint8(len(upperAlphaNum)))]
+	}
+	return b
+}
+
+func (g *Generator) UpperAlpha(n int) []byte {
+	b := make([]byte, n)
+	for i := 0; i < n; i++ {
+		b[i] = upperAlpha[g.intN(uint8(len(upperAlpha)))]
+	}
+	return b
+}
+
+func (g *Generator) ASCII64(n int) []byte {
+	b := make([]byte, n)
+	for i := 0; i < n; i++ {
+		b[i] = upperAlpha[g.intN(uint8(len(upperAlpha)))]
+	}
+	return b
+}
+
+// read fills the cache.
+func (g *Generator) read() {
+	_, err := rand.Read(g.cache)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// intN gets an unbiased value from the cache of random byte values.
+func (g *Generator) intN(bound uint8) int {
+	threshold := -bound % bound
+	for {
+		n := g.cache[g.current]
+		g.current++
+		// if we're at the end; replenish the cache
+		if g.current >= g.cacheSize {
+			g.read()
+			g.current = 0
+		}
+		if n >= threshold {
+			return int(n % bound)
+		}
+	}
+}
---
+++ crandchars/crandchars_test.go
@@ -0,0 +1,194 @@
+package crandchars
+
+import (
+	"testing"
+)
+
+func BenchmarkAlphaNum_8(b *testing.B) {
+	g := New()
+	for i := 0; i < b.N; i++ {
+		g.AlphaNum(8)
+	}
+}
+
+func BenchmarkLowerAlpha_8(b *testing.B) {
+	g := New()
+	for i := 0; i < b.N; i++ {
+		g.LowerAlpha(8)
+	}
+}
+
+func BenchmarkLowerAlphaNum_8(b *testing.B) {
+	g := New()
+	for i := 0; i < b.N; i++ {
+		g.LowerAlphaNum(8)
+	}
+}
+
+func BenchmarkUpperAlpha_8(b *testing.B) {
+	g := New()
+	for i := 0; i < b.N; i++ {
+		g.UpperAlpha(8)
+	}
+}
+
+func BenchmarkUpperAlphaNum_8(b *testing.B) {
+	g := New()
+	for i := 0; i < b.N; i++ {
+		g.UpperAlphaNum(8)
+	}
+}
+
+func BenchmarkASCII64_8(b *testing.B) {
+	g := New()
+	for i := 0; i < b.N; i++ {
+		g.ASCII64(8)
+	}
+}
+
+func BenchmarkAlpha_16(b *testing.B) {
+	g := New()
+	for i := 0; i < b.N; i++ {
+		g.Alpha(16)
+	}
+}
+
+func BenchmarkAlphaNum_16(b *testing.B) {
+	g := New()
+	for i := 0; i < b.N; i++ {
+		g.AlphaNum(16)
+	}
+}
+
+func BenchmarkLowerAlpha_16(b *testing.B) {
+	g := New()
+	for i := 0; i < b.N; i++ {
+		g.LowerAlpha(16)
+	}
+}
+
+func BenchmarkLowerAlphaNum_16(b *testing.B) {
+	g := New()
+	for i := 0; i < b.N; i++ {
+		g.LowerAlphaNum(16)
+	}
+}
+
+func BenchmarkUpperAlpha_16(b *testing.B) {
+	g := New()
+	for i := 0; i < b.N; i++ {
+		g.UpperAlpha(16)
+	}
+}
+
+func BenchmarkUpperAlphaNum_16(b *testing.B) {
+	g := New()
+	for i := 0; i < b.N; i++ {
+		g.UpperAlphaNum(16)
+	}
+}
+
+func BenchmarkASCII64_16(b *testing.B) {
+	g := New()
+	for i := 0; i < b.N; i++ {
+		g.ASCII64(16)
+	}
+}
+
+func BenchmarkAlpha_32(b *testing.B) {
+	g := New()
+	for i := 0; i < b.N; i++ {
+		g.Alpha(32)
+	}
+}
+
+func BenchmarkAlphaNum_32(b *testing.B) {
+	g := New()
+	for i := 0; i < b.N; i++ {
+		g.AlphaNum(32)
+	}
+}
+
+func BenchmarkLowerAlpha_32(b *testing.B) {
+	g := New()
+	for i := 0; i < b.N; i++ {
+		g.LowerAlpha(32)
+	}
+}
+
+func BenchmarkLowerAlphaNum_32(b *testing.B) {
+	g := New()
+	for i := 0; i < b.N; i++ {
+		g.LowerAlphaNum(32)
+	}
+}
+
+func BenchmarkUpperAlpha_32(b *testing.B) {
+	g := New()
+	for i := 0; i < b.N; i++ {
+		g.UpperAlpha(32)
+	}
+}
+
+func BenchmarkUpperAlphaNum_32(b *testing.B) {
+	g := New()
+	for i := 0; i < b.N; i++ {
+		g.UpperAlphaNum(32)
+	}
+}
+
+func BenchmarkASCII64_32(b *testing.B) {
+	g := New()
+	for i := 0; i < b.N; i++ {
+		g.ASCII64(32)
+	}
+}
+
+func BenchmarkAlpha_64(b *testing.B) {
+	g := New()
+	for i := 0; i < b.N; i++ {
+		g.Alpha(64)
+	}
+}
+
+func BenchmarkAlphaNum_64(b *testing.B) {
+	g := New()
+	for i := 0; i < b.N; i++ {
+		g.AlphaNum(64)
+	}
+}
+
+func BenchmarkLowerAlpha_64(b *testing.B) {
+	g := New()
+	for i := 0; i < b.N; i++ {
+		g.LowerAlpha(64)
+	}
+}
+
+func BenchmarkLowerAlphaNum_64(b *testing.B) {
+	g := New()
+	for i := 0; i < b.N; i++ {
+		g.LowerAlphaNum(64)
+	}
+}
+
+func BenchmarkUpperAlpha_64(b *testing.B) {
+	g := New()
+	for i := 0; i < b.N; i++ {
+		g.UpperAlpha(64)
+	}
+}
+
+func BenchmarkUpperAlphaNum_64(b *testing.B) {
+	g := New()
+	for i := 0; i < b.N; i++ {
+		g.UpperAlphaNum(64)
+	}
+}
+
+func BenchmarkASCII64_64(b *testing.B) {
+	g := New()
+	for i := 0; i < b.N; i++ {
+		g.ASCII64(64)
+	}
+}
